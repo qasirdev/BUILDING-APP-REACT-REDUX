069f25cfbab7ff35fe3b533af3e9484d692e435d
Qasir Mehmood <qasirdev@gmail.com>
2021-11-16 22:36:39 +0000

    manage course page with class component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./courses/CoursesPage";
+import ManageCoursePage from "./courses/ManageCoursePage";
 import HomePage from "./home/HomePage";
 import PageNotFound from "./PageNotFound";
 
@@ -14,6 +15,8 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+
+class MangeCoursePage extends React.Component {
+  componentDidMount() {
+    const { courses, authors, loadCourses, loadAuthors } = this.props;
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert("Loading courses failed" + error);
+      });
+    }
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed" + error);
+      });
+    }
+  }
+  render() {
+    return (
+      <>
+        <h2>Manage Courses</h2>
+      </>
+    );
+  }
+}
+
+MangeCoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors,
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MangeCoursePage);
